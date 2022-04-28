02b8a0bbcd3aac5872cc14c62567bc4505a5c451
barisozakar <barisozakar@gmail.com>
2022-04-28 10:35:22 +0200

    implemented downloadDataset for Yahoo API Download functionality

--- FinancialEnvLayer/test_dowloader.py
+++ FinancialEnvLayer/test_dowloader.py
@@ -1,8 +1,15 @@
 # Test File
 from customDataset import ImportCustomDataset
+from downloadDataset import DownloadDataset
 import pandas as pd
 
 if __name__ == '__main__':
 
-    df = ImportCustomDataset("examplejson.json").createDataset()
-    print(df)
+    # df = ImportCustomDataset("examplejson.json").createDataset()
+    # print(df)
+
+    tickers = ['AAPL', 'DOW']  # ticker types defined
+    df = DownloadDataset(start_date='2009-01-01',
+                         end_date='2021-10-31',
+                         ticker_list=tickers).createDataset()  # get data from Yahoo Downloader
+    print(df.head())
