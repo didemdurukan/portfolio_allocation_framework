c29652fc967fb535748ab58bcbd74b1334698d65
doganparlak <dgnprlk@gmail.com>
2022-05-31 22:55:20 +0200

    random forest agent added and tested

---
+++ AgentLayer/Testdir/RF_test.py
@@ -0,0 +1,67 @@
+import yaml
+from AgentLayer.ConventionalAgents.RFAgent import RFAgent
+from FinancialDataLayer.DataCollection.DataDownloader import DataDownloader
+from FinancialDataLayer.DataProcessing.DefaultFeatureEngineer import DefaultFeatureEngineer
+from AgentLayer.DataSplitter.TimeSeriesSplitter import TimeSeriesSplitter
+
+if __name__ == '__main__':
+
+    # IMPORT .yaml FILE
+    # Gather user parameters
+    with open("user_params.yaml", "r") as stream:
+        try:
+            user_params = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    tickers = user_params["tickers"]
+    env_kwargs = user_params["env_params"]
+    train_params = user_params["train_params"]
+    policy_params = user_params["policy_params"]
+    test_params = user_params["test_params"]
+
+    # FETCH DATA
+    print("\nTest 3: Downloading from Yahoo.........")
+    downloaded_df = DataDownloader(start_date='2009-01-01',
+                                   end_date='2021-10-31',
+                                   ticker_list=tickers).download_from_yahoo()
+    """
+    downloaded_df = DataDownloader.download_data(start_date='2009-01-01',
+                                                    end_date='2021-10-31',
+                                                ticker_list=tickers)
+    """
+    print(downloaded_df.head())
+
+    # PREPROCESS DATA
+    print("\nTest 4: Feature engineer.........")
+    data_processor = DefaultFeatureEngineer(use_default=False,
+                                            tech_indicator_list=env_kwargs["tech_indicator_list"],
+                                            use_vix=True,
+                                            use_turbulence=True,
+                                            use_covar=True)
+    # included technical indicators as features
+    df_processed = data_processor.extend_data(downloaded_df)
+
+    # split data to train and test
+    splitter = TimeSeriesSplitter()
+    train = splitter.get_split_data(df_processed, '2009-01-01', '2020-06-30')
+    trade = splitter.get_split_data(df_processed, '2020-07-01', '2021-09-02')
+
+    #prepare conventional data
+    train_x, train_y = data_processor.prepare_ml_data(train)
+
+    # create agent
+    rf = RFAgent(**policy_params["RF_PARAMS"])
+
+    #train agent
+    rf.train_model(train_x, train_y, **train_params["RF_PARAMS"])
+
+    #predict 
+    portfolio, portfolio_cumprod, meta_coefficient = rf.predict(trade, **test_params["RF_PARAMS"])
+    print(portfolio)
+
+    #save model
+    rf.save_model("AgentLayer/ConventionalAgents/rf_model")
+
+    #load model
+    rf_loaded = rf.load_model("AgentLayer/ConventionalAgents/rf_model")
